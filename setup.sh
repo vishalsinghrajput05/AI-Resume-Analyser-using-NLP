8fc359f8834561c31b0fed4471db1e9342886710
Vishal Raj <156817423+vishalsinghrajput05@users.noreply.github.com>
2025-09-07 17:14:08 +0530

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.nltk_data
-
 python -m nltk.downloader -d ~/.nltk_data punkt
 python -m nltk.downloader -d ~/.nltk_data averaged_perceptron_tagger
 python -m nltk.downloader -d ~/.nltk_data maxent_ne_chunker
