ada7c82521e87bd807be287674a290669a430cde
Vishal Raj <156817423+vishalsinghrajput05@users.noreply.github.com>
2025-09-07 15:30:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python -m spacy download en_core_web_sm
+mkdir -p ~/.nltk_data
+python -m nltk.downloader -d ~/.nltk_data punkt averaged_perceptron_tagger maxent_ne_chunker words stopwords
