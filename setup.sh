8d29fdd731d2dfc5b3af262d2e65321a7c8f768f
Vishal Raj <156817423+vishalsinghrajput05@users.noreply.github.com>
2025-09-07 15:34:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 mkdir -p ~/.nltk_data
-python -m nltk.downloader -d ~/.nltk_data punkt averaged_perceptron_tagger maxent_ne_chunker words stopwords
+
+python -m nltk.downloader -d ~/.nltk_data punkt
+python -m nltk.downloader -d ~/.nltk_data averaged_perceptron_tagger
+python -m nltk.downloader -d ~/.nltk_data maxent_ne_chunker
+python -m nltk.downloader -d ~/.nltk_data words
+python -m nltk.downloader -d ~/.nltk_data stopwords
